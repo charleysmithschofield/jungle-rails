52ef2abcbd95d8a137e3aa06b8ac7446bb72d700
charleysmithschofield <charleysmithschofield@gmail.com>
2024-07-06 15:53:13 -0700

    add dashboard counts

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,8 +1,6 @@
 class Admin::DashboardController < ApplicationController
-
-  before_action :authorize
-
   def show
+    @product_count = Product.count
+    @category_count = Category.count
   end
-  
 end
