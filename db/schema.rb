1105e2b7b047b4a5f6c9fc47349a9edce01d6870
charleysmithschofield <charleysmithschofield@gmail.com>
2024-07-06 15:40:46 -0700

    re-run db migration file to correct schema to include first_name and last_name instead of name

--- db/schema.rb
+++ db/schema.rb
@@ -54,9 +54,10 @@ ActiveRecord::Schema.define(version: 2024_07_06_213418) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "name"
+    t.string "first_name"
+    t.string "last_name"
     t.string "email"
-    t.string "password_disgest"
+    t.string "password_digest"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
